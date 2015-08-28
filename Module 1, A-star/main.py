7ae1b611e62a1780212e84e1598e81b48ee33181
Ilse Visser <irxess@gmail.com>
2015-08-28 22:45:33 +0200

    Draw walls with info from imput file

--- Module 1, A-star/main.py
+++ Module 1, A-star/main.py
@@ -7,18 +7,30 @@ def main():
     stdin = []
     translation_table = dict.fromkeys(map(ord, '() \n'), None)
     for line in sys.stdin:
-    	stdin.append(line.translate(translation_table).split(','))
+        # translate )( to ,
+        line = line.replace(') (', ',')
+        line = line.replace(')(', ',')
+        stdin.append(line.translate(translation_table).split(','))
+        stdin[-1] = [ int(x) for x in stdin[-1] ]
 
     rows = stdin[0][0]
     columns = stdin[0][1]
-    grid = w.create_grid(int(rows), int(columns))
-    grid.update_cell(int(stdin[1][0]), int(stdin[1][1]), 'start')
-    grid.update_cell(int(stdin[2][0]), int(stdin[2][1]), 'goal')
+    grid = w.create_grid(rows, columns)
+    grid.update_cell(stdin[1][0], stdin[1][1], 'start')
+    grid.update_cell(stdin[2][0], stdin[2][1], 'goal')
 
+    # for each wall
+    for i in range(0, len(stdin[3])-1, 4):
+        # for each piece in row
+        for row_offset in range(stdin[3][i+2]):
+        	# for each piece in column
+            for col_offset in range(stdin[3][i+3]):
+                x = stdin[3][i] + row_offset
+                y = stdin[3][i + 1] + col_offset
+                grid.update_cell(x, y, 'blocked')
 
 
     w.loop()
-
 
 
 if __name__ == "__main__":
