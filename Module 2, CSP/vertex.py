f539524b0eb21290e374c3cc21cdc2c243c307d2
Ilse Visser <irxess@gmail.com>
2015-09-29 13:58:26 +0200

    Fix indentation

--- Module 2, CSP/vertex.py
+++ Module 2, CSP/vertex.py
@@ -1,9 +1,9 @@
 class Vertex:
     def __init__(self, x, y):
-    	self.x = x
-    	self.y = y
-    	self.neighbors = []
-    	self.color = (0,0,0)
+        self.x = x
+        self.y = y
+        self.neighbors = []
+        self.color = (0,0,0)
         # domain:set of values for vertex
         self.domain = [] #maybe use a dictionary instead?
         self.constraints = {}
@@ -12,19 +12,19 @@ class Vertex:
         return 'vertex(x=%s, y=%s, color=%s, domain=%s' %(self.x, self.y, self.color. self.domain)
 
     def add_neighbor(self, vertex):
-    	self.neighbors.append(vertex)
+        self.neighbors.append(vertex)
     
     def getNeighbors(self):
         return self.neighbors
 
     def setColor(self, color):
-    	self.color = color
+        self.color = color
 
     def getColor(self):
-    	return self.color
+        return self.color
 
     def getPosition(self):
-    	return (self.x,self.y)
+        return (self.x,self.y)
 
     def getDomain(self):
         return self.domain
