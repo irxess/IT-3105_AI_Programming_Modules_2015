4b3fce994e97715f8ffde3c0bafe7ce3506f8afa
Neshat Naderi <neshat.dmd@gmail.com>
2015-10-02 17:36:19 +0200

    Deleted state arg. from revise

--- Module 2, CSP/gac.py
+++ Module 2, CSP/gac.py
@@ -2,6 +2,7 @@ from copy import deepcopy
 import itertools
 from state import *
 from constraintInstance import CI
+import pdb
 
 class GAC():
 
@@ -19,6 +20,7 @@ class GAC():
         self.queue = [] # queue of requests(focal variable, its constraints), initially all requests
         self.constraints = state.ciList
         self.variables = state.viList
+        # self.state = state
 
     def initialize(self):
         print('Put all constraints + variables in the GAC queue')
@@ -27,17 +29,16 @@ class GAC():
                 self.queue.append((x, c))
 
 
-    def filterDomain(self, state):             
+    def filterDomain(self, state):           
         while len(self.queue):
             (x, c) = self.queue.pop()
-            print("todoRevise:", (x,c))
+            print("todoRevise var:", (x, c))
             # er x.domain en liste?
-            print 
-            print("var to filter & const.v:", x, c.variables)
-            if self.reviseStar(state, x, c):
+            if self.reviseStar(x, c):
                 if len( x.domain ) == 0:
                     print ("inconsistency", x.domain, "reduced to empty")
                     return False
+                print('domlengde til x: ', len(x.domain))
                 self.rerun(x)
         print("vars in currState after filterDomain:", self.variables)
         return state
@@ -55,39 +56,52 @@ class GAC():
 
 
 # reduce x's domain
-    def reviseStar(self, state, x, c):
+    def reviseStar(self, x, c):
         revised = False
+        print("(c) :",c)
+        # pdb.set_trace()
         pairs = self.getPairs(x, c.variables)
         for listOfPairs in pairs:
             satisfiedCount = 0
+            print("pairList in revise:", listOfPairs)
             for pair in listOfPairs:
+                print("checking pair: ", pair)
                 if self.isSatisfied(pair, c):
-                    # print("satisfied:",self.isSatisfied(pair, c))
+                    print("satisfied:",self.isSatisfied(pair, c))
                     satisfiedCount += 1
-                # else:
-            if satisfiedCount == 0:
+                # else: 
+                if satisfiedCount == 0:
+                    print("notSatisfied:",self.isSatisfied(pair, c))
+
                 # remove the variable from the domain,
                 # as there is no combination with the variable 
                 # where the constraint is satisfied
+                    print('x.domain',x.domain)
                     print('Remove', pair[0], 'from the domain of', x)
                     x.domain.remove(pair[0])
-                    self.reduceDomain(state, x, pair[0])
+                    print('x.domain',x.domain)
+
+                    print('state.variables:', state.viList)
+                    # self.reduceDomain(state, x, pair[0])
                     revised = True
+                    return revised
+
         return revised
 
 # assumption: a variable assignment/singleton domain
     def rerun(self, assumption):
         for c in self.getConstraints(assumption):
             for k in c.variables:
+                print('k != assumption',k != assumption)
                 if k != assumption:
-                    self.queue.append((k, getConstraints(k)))
+                    self.queue.append((k, self.getConstraints(k)))
         return self.filterDomain(assumption)
 
     def isSatisfied(self, pair, constraint):
         return constraint.constraint(pair[0], pair[1])
         
     def getPairs(self, x, y):
-        pairs = []
+        pairs = [] 
         for k in y:
             if k != x:
                 for value in x.domain:
@@ -107,8 +121,9 @@ class GAC():
     def reduceDomain(self, state, vi, item):
         for v in state.viList:
             if v == vi:
-                print(v.domain)
+                print("vi.domain", vi.domain)
                 print(item)
-                vi.domain.remove(item)
+                print('v.domain', v.domain)
+                v.domain.remove(item)
 
         
