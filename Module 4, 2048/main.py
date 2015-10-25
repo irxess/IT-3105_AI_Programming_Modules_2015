b09db1f456be495378c4c4edc8d8a4837372c035
Ilse Visser <irxess@gmail.com>
2015-10-26 00:02:13 +0100

    Update prints for testing

--- Module 4, 2048/main.py
+++ Module 4, 2048/main.py
@@ -4,6 +4,7 @@ from expectimax import *
 from copy import copy
 import time, timeit
 import settings, sys
+import state
 
 # measure process time
 # t0 = time.clock()
@@ -17,6 +18,15 @@ settings.init(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], s
 b = window()
 b.window.update_view(b.board)
 
+
+def emptyTiles(board):
+    count = 0
+    for cell in board:
+        if cell == 0:
+            count += 1
+    return count
+
+
 def logic():
     bestHeuristic = 0
     bestDirection = 'none'
@@ -24,8 +34,13 @@ def logic():
     for direction in ['up', 'down', 'left', 'right']:
         nextBoard, nofMerges = bc.slide( direction, copy(b.board) )
         if nextBoard != b.board:
-
-            heuristic = expectimax( nextBoard, 5, 'board', nofMerges)
+            nofEmpty = emptyTiles(nextBoard)
+            if nofEmpty > 10:
+                heuristic = expectimax( nextBoard, 2, 'board', nofMerges)
+            elif nofEmpty > 5:
+                heuristic = expectimax( nextBoard, 4, 'board', nofMerges)
+            else:
+                heuristic = expectimax( nextBoard, 6, 'board', nofMerges)
             if heuristic > bestHeuristic:
                 bestHeuristic = heuristic
                 bestDirection = direction
@@ -40,7 +55,7 @@ def logic():
         # print 'Running time: ', time.clock()
         # while True:
             # b.window.update_view(b.board)
-        print 2**max(b.board)
+        print 2**max(b.board), ' |\t', sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6]
         sys.exit(0)
 
 while True:
